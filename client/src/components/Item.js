a2d3b597bf4a6e1ab6276ffce0d9d6ec19f40e9b
AashaiAvadhani1 <aashai123@gmail.com>
2022-04-07 01:01:05 -0400

    added the components for the app.js

---
+++ client/src/components/Item.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Item({ row }) {
+  return (
+    <li className="item">
+      <div className="item__avatar">
+        <img
+          className="item__avatar__img"
+          alt={row.displayName}
+        />
+      </div>
+      <span className="item__name">{row.displayName}</span>
+      <span className="item__score">{row.score}</span>
+    </li>
+  );
+}
---
+++ client/src/components/Leaderboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Item from "./Item";
+
+/**
+ * The method that can be called to pass the data into a set list per item. 
+ * Where an item is a row 
+ * @param {*} param0 
+ * @returns 
+ */
+export default function List({ data }) {
+  return (
+    <ul className="item-wrapper">
+      {data.map(row => (
+        <Item row={row} />
+      ))}
+    </ul>
+  );
+}
